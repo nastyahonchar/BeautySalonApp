e0e74fa2e0b0ecf21e9fa25a6151a4956244aa7d
Anastasia Honchar <honchar.anastasiia@chnu.edu.ua>
2026-04-23 17:09:16 +0300

    Configured dependency injection and services

--- BeautySalon.API/Program.cs
+++ BeautySalon.API/Program.cs
@@ -1,24 +1,40 @@
 using Microsoft.EntityFrameworkCore;
+using BeautySalon.BLL.Profiles;
+using BeautySalon.BLL.Services;
+using BeautySalon.BLL.Services.Interfaces;
 using BeautySalon.DAL.Data;
+using BeautySalon.DAL.Repositories;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<BeautySalonDBContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<EmployeeServiceRepository>();
+
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IServiceService, ServiceService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IEmployeeServiceService, EmployeeServiceService>();
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
